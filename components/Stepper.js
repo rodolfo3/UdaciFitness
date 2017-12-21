629b82cbf039458ccb36dc7c0e4f774c436ecb75
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2017-12-21 11:41:45 -0200

    Style Stepper

--- components/Stepper.js
+++ components/Stepper.js
@@ -1,22 +1,63 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native'
 import { FontAwesome, Entypo } from '@expo/vector-icons'
+
+import { white, gray, purple } from '../utils/colors'
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iosBtn: {
+    backgroundColor: white,
+    borderColor: purple,
+    borderWidth: 1,
+    borderRadius: 3,
+    padding: 5,
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+  androidBtn: {
+    margin: 5,
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 2,
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+
+const Icon = Platform.OS === 'ios' ? Entypo : FontAwesome;
+const styleBtn = Platform.OS === 'ios' ? styles.iosBtn : styles.androidBtn;
+const iconColor = Platform.OS === 'ios' ? purple : white;
 
 export default function Stepper ({ max, unit, step, value, increment, decrement }) {
   return (
-    <View>
-      <View>
-        <TouchableOpacity onPress={decrement}>
-          <FontAwesome name='minus' size={30} color={'black'} />
+    <View style={[styles.row, {justifyContent: 'space-between'}]}>
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity
+          style={[styleBtn, {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}
+          onPress={decrement}
+        >
+          <Icon name='minus' size={30} color={iconColor} />
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={increment}>
-          <FontAwesome name='plus' size={30} color={'black'} />
+        <TouchableOpacity
+          style={[styleBtn, {borderTopLeftRadius: 0, borderBottomLeftRadius: 0}]}
+          onPress={increment}
+        >
+          <Icon name='plus' size={30} color={iconColor} />
         </TouchableOpacity>
-        <View>
-            <Text>{ value }</Text>
-            <Text>{ unit }</Text>
-        </View>
+      </View>
+      <View style={styles.metricCounter}>
+          <Text style={{ fontSize: 24, textAlign: 'center'}}>{ value }</Text>
+          <Text style={{ fontSize: 18, textAlign: 'center', color: gray}}>{ unit }</Text>
       </View>
     </View>
   );
