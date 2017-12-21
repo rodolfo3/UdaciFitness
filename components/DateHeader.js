df4fd3cf2ced78e6563b51f5c6ee3b8e3911f0fd
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2017-12-21 12:01:01 -0200

    Style DateHeader

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { View, Text} from 'react-native'
 
+import { purple } from '../utils/colors'
+
+
 export default function DateHeader ({ date }) {
   const dateStr = String(date.toLocaleDateString())
   return (
-    <Text>
+    <Text style={{color: purple, fontSize: 25}}>
       { dateStr }
     </Text>
   );
