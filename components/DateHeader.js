2666df61871430ae00f227a5fa171ce7a4e587b7
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2017-12-29 10:21:52 -0200

    Allow DateHeader to receive dates and strings

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -5,7 +5,7 @@ import { purple } from '../utils/colors'
 
 
 export default function DateHeader ({ date }) {
-  const dateStr = String(date.toLocaleDateString())
+  const dateStr = date.toLocaleDateString ? String(date.toLocaleDateString()) : String(date)
   return (
     <Text style={{color: purple, fontSize: 25}}>
       { dateStr }
