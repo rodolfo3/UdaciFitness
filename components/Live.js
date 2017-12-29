fa2f344404efc39482a824ba738020267dff3b52
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2017-12-29 15:43:58 -0200

    Add stryle for Live status (denied/undetermined)

--- components/Live.js
+++ components/Live.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, ActivityIndicator, StyleSheet } from 'react-native'
+import { Foundation } from '@expo/vector-icons'
+import { Location } from 'expo'
 import { connect } from 'react-redux'
+
+import { purple, white } from '../utils/colors'
 
 
 const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
 })
 
 
 class Live extends Component {
   state = {
     coords: null,
-    status: null,
+    status: 'undetermined',
     direction: '',
+  }
+
+
+  askPermission = () => {
+    console.log('askPermission');
   }
 
   render() {
@@ -27,16 +41,28 @@ class Live extends Component {
 
     if (status === 'denied') {
       return (
-        <View style={styles.container}>
-          <Text>Denied!</Text>
+        <View style={styles.center}>
+          <Foundation name='alert' size={50} />
+          <Text>
+            You denied location.
+            You can fix this by visiting your settings... bla
+          </Text>
         </View>
       )
     }
 
     if (status === 'undetermined') {
       return (
-        <View style={styles.container}>
-          <Text>Undetermined!</Text>
+        <View style={styles.center}>
+          <Foundation name='alert' size={50} />
+          <Text>
+            You need to enable location services dor this app
+          </Text>
+          <TouchableOpacity onPress={this.askPermission}>
+            <Text>
+              Enable
+            </Text>
+          </TouchableOpacity>
         </View>
       )
     }
